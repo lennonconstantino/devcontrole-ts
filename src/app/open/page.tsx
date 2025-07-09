338b1589ae23a500bdfe952366a2f0fd32b01ce7
lennonconstantino <lennonconstantino@gmail.com>
2025-07-09 11:59:22 -0300

    form open ticket

--- src/app/open/page.tsx
+++ src/app/open/page.tsx
@@ -5,7 +5,8 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
-import { FiSearch } from 'react-icons/fi'
+import { FiSearch, FiX } from 'react-icons/fi'
+import { FormTicket } from './components/FormTicket'
 
 const schema = z.object({
   email: z.string().email("Digite o email do cliente para localizar.").min(1, "O campo email é obrigatório.")
@@ -19,14 +20,16 @@ interface CustomerDataInfo {
 }
 
 export default function OpenTicket() {
-  const [customer, setCustomer] = useState<CustomerDataInfo | null>({
-    id: "1",
-    name: "JOSE"
-  })
+  const [customer, setCustomer] = useState<CustomerDataInfo | null>(null)
 
   const { register, handleSubmit, setValue, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(schema)
   })
+
+  function handleClearCustomer() {
+    setCustomer(null)
+    setValue("email", "")
+  }
 
 
   return (
@@ -36,8 +39,11 @@ export default function OpenTicket() {
       <main className="flex flex-col mt-4 mb-2">
 
         {customer ? (
-          <div>
-            Wow
+          <div className="bg-slate-200 py-6 px-4 rounded border-2 flex items-center justify-between">
+            <p className="text-lg"><strong>Cliente selecionado:</strong> {customer.name}</p>
+            <button className="h-11 px-2 flex items-center justify-center rounded" onClick={handleClearCustomer}>
+              <FiX size={30} color="#ff2929" />
+            </button>
           </div>
         ) : (
           <form className="bg-slate-200 py-6 px-2 rounded border-2">
@@ -57,6 +63,8 @@ export default function OpenTicket() {
             </div>
           </form>
         )}
+
+        {customer !== null && <FormTicket />}
 
 
       </main>
